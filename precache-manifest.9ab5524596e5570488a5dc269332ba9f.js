5e589d2487e2736e3c38b8b05b327da0ff7c60e6
HardRain <nikolaimihail@gmail.com>
2020-10-14 23:45:17 +0300

    Updates

---
+++ precache-manifest.9ab5524596e5570488a5dc269332ba9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158dd38a443c0426e1ad6351ba83ef75",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "8bbf19347f2f24c75956",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.e22e57cd.chunk.css"
+  },
+  {
+    "revision": "5e06be513555a05b22e2",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.daac1b38.chunk.css"
+  },
+  {
+    "revision": "8bbf19347f2f24c75956",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.21828ead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.21828ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e06be513555a05b22e2",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.d3a3432a.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
